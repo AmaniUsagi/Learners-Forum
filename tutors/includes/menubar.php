8d3c6e5bb769cc9d0ab0597139319ae438b50336
Amani_06 <38758226+AmaniUsagi@users.noreply.github.com>
2018-10-11 18:34:28 +0300

    edit
    Unecessary item removed

--- tutors/includes/menubar.php
+++ tutors/includes/menubar.php
@@ -4,6 +4,7 @@
             <div class="col-md-12">
                 <div class="navbar-collapse collapse ">
                     <ul id="menu-top" class="nav navbar-nav navbar-right">
+                        <li><a href=""><i class="fa fa-book-open"></i> Forum</a></li>
                         <li><a href="pincode-verification.php"><span class="glyphicon glyphicon-list"></span> Unit Enrolment </a></li>
                         <li><a href="enroll-history.php"><i class="fa fa-book"></i> Enrolled Units</a></li>
                         <li><a href="#"><i class="glyphicon glyphicon-education"></i> Students</a></li>

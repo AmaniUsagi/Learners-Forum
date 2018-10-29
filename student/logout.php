0fb4ba6963e314e4caa7bc81082df28cd7ae4a4c
Amani_06 <38758226+AmaniUsagi@users.noreply.github.com>
2018-10-29 16:07:01 +0300

    edit

--- student/logout.php
+++ student/logout.php
@@ -5,7 +5,7 @@ $_SESSION['login']=="";
 date_default_timezone_set('Africa/Nairobi');
 $ldate=date( 'd-m-Y h:i:s A', time () );
 mysqli_query($con,"UPDATE userlog  SET logout = '$ldate' WHERE studentRegno = '".$_SESSION['login']."' ORDER BY id DESC LIMIT 1");
-session_unset();
+session_unset($_SESSION['login']);
 $_SESSION['errmsg']="Successfully logged out!";
 ?>
 <script language="javascript">
